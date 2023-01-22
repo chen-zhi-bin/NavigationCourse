93da534647c66910aaff362f0e7e76c5e1589eab
precontext <815172923@qq.com>
2023-01-22 17:06:28 +0800

    init

---
+++ app/src/main/java/com/program/navigationcourse/fragment/RegisterFragment.kt
@@ -0,0 +1,10 @@
+package com.program.navigationcourse.fragment
+
+import com.program.navigationcourse.R
+import com.program.navigationcourse.base.BaseFragment
+
+class RegisterFragment:BaseFragment() {
+    override fun getLayoutResId(): Int {
+        return R.layout.fragment_register
+    }
+}
