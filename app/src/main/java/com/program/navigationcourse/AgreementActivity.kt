03b64125233ef8beb5427e63dbae93137b6e1aa1
precontext <815172923@qq.com>
2023-01-23 15:42:22 +0800

    添加切换fragment和跳转activity

---
+++ app/src/main/java/com/program/navigationcourse/AgreementActivity.kt
@@ -0,0 +1,13 @@
+package com.program.navigationcourse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgreementActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agreement)
+    }
+
+}
--- app/src/main/java/com/program/navigationcourse/fragment/LoginFragment.kt
+++ app/src/main/java/com/program/navigationcourse/fragment/LoginFragment.kt
@@ -1,10 +1,30 @@
 package com.program.navigationcourse.fragment
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.program.navigationcourse.R
 import com.program.navigationcourse.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 class LoginFragment:BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_login
+    }
+
+    override fun initView(rootView: View) {
+        super.initView(rootView)
+        rootView.toRegisterPage.setOnClickListener {
+            //跳转到注册界面
+            findNavController().navigate(R.id.to_register_fragment)
+        }
+        rootView.toForgetPage.setOnClickListener {
+            //跳转到找回密码界面
+            findNavController().navigate(R.id.to_forget_fragment)
+        }
+        rootView.toAgreementPage.setOnClickListener {
+            //跳转到用户协议
+            findNavController().navigate(R.id.to_agreement_activity)
+        }
+
     }
 }
